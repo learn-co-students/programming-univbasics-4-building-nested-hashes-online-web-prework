da25b8b1c318385946fb85791d8be42c331ae524
Darin Webster <darinleenavy@gmail.com>
2019-06-18 19:33:29 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,19 @@
 def monopoly
 	monopoly = {}
+  
+  monopoly[{}] = :railroads
 end
 
 def monopoly_with_second_tier
   monopoly_with_second_tier = {
 	  :railroads => {
-	    :pieces => 4
+	    :pieces => 0
 	  }
   }
+  
+monopoly_with_second_tier[:railroads][:pieces] = 4
+puts monopoly_with_second_tier
+
 end
 
 def monopoly_with_third_tier
@@ -15,14 +21,27 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4, 
 	    :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_piece_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200},
+	      :one_piece_owned => 0,
+	      :two_piece_owned => 0,
+	      :three_pieces_owned => 0,
+	      :four_pieces_owned => 0},
 	  :name => {
 	  }
   }
 }
+
+monopoly_with_third_tier[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+puts monopoly_with_third_tier
+
+monopoly_with_third_tier[:railroads][:rent_in_dollars][:two_piece_owned] = 50
+puts monopoly_with_third_tier
+
+monopoly_with_third_tier[:railroads][:rent_in_dollars][:three_piece_owned] = 100
+puts monopoly_with_third_tier
+
+monopoly_with_third_tier[:railroads][:rent_in_dollars][:four_piece_owned] = 200
+puts monopoly_with_third_tier
+
 end
 
 def monopoly_with_fourth_tier
