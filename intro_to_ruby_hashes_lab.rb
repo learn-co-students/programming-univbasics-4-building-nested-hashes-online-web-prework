f0c1f6e9062d8bc67711e62dc121a6d53108ca80
yassir bennani <ybennani@yahoo.com>
2019-06-25 21:13:32 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,37 @@
 def monopoly
-	monopoly = {}
+	monopoly = {
+		railroads=>
+	  {
+	    :pieces=>4,
+	    :rent_in_dollars=>
+	    {
+	      :one_piece_owned=>25,
+	      :two_pieces_owned=>50,
+	      :three_pieces_owned=>100,
+	      :four_pieces_owned=>200
+	    },
+	    :names=>
+	    {
+	      :reading_railroad=>
+	      {
+	        "mortgage_value"=>"$100"
+	      },
+	      :pennsylvania_railroad=>
+	      {
+	        "mortgage_value"=>"$200"
+	      },
+	      :b_and_o_railroad=>
+	      {
+	        "mortgage_value"=>"$400"
+	      },
+	      :shortline=>
+	      {
+	        "mortgage_value"=>"$800"
+	      }
+	    }
+	  }
+	}
+	}
 
 end
 
