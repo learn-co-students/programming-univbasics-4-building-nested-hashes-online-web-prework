58b716c0ea80121e15ed9ee4693877011a4fb3bd
Shahwar Spall <shevy.spall@gmail.com>
2019-07-08 03:27:31 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,9 @@ def monopoly_with_second_tier
  :railroads=>
 	  {
 	  :pieces=>4,
+	  :names=> {},
     :rent_in_dollars=> {},
-    :name=> {}
+    
 	  }
   }
 
@@ -21,8 +22,14 @@ def monopoly_with_third_tier
  monopoly = {
     :railroads => {
       :pieces => 4,
-      :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}},
+      :names => {
+        :reading_railroad => {}, 
+        :pennsylvania_railroad => {}, 
+        :b_and_o_railroad => {}, 
+        :shortline => {}},
       :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+      
+     
     }
   }
 
