0d3236c9c188fafaf3ce8bcc3579f994fc9a2ec2
Steven Laff <steve.laff@gmail.com>
2019-08-07 02:30:51 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,13 +22,11 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars =>{
-        :pieces_owned =>{
-          :one_piece => 25,
-          :two_pieces => 50,
-          :three_pieces => 100,
-          :four_pieces => 200
-        } 
-      }
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 100,
+          :four_pieces_owned => 200
+      },
       :names =>{
         :reading_railroad => {},
         :pennsylvania_railroad => {},
@@ -48,13 +46,11 @@ def monopoly_with_fourth_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars =>{
-        :pieces_owned =>{
-          :one_piece => 25,
-          :two_pieces => 50,
-          :three_pieces => 100,
-          :four_pieces => 200
-        } 
-      }
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 100,
+          :four_pieces_owned => 200
+      },
       :names =>{
         :reading_railroad => {"mortgage_value" => "$100"},
         :pennsylvania_railroad => {"mortgage_value" => "$200"},
