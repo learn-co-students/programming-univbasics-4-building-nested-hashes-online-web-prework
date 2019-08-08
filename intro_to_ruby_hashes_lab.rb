a00e7e978425e849898901291e2eb89f247199f3
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 17:39:09 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,62 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  monoploy_board = {
+
+	}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly_board ={
+		:main_line => {
+
+			},
+		},
+		:stream_line => {
+
+	}
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	monopoly_board ={
+		:main_line => {
+			"Red" => {
+
+			},
+			"Yellow" => {
+
+			},
+		},
+		:stream_line => {
+			"Blue" => {
+
+			},
+			"Green" => {
+
+			},
+		},
+	}
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	monopoly_board ={
+		:main_line => {
+			"Red" => {
+				:main_street => "Illinois Ave.",
+				:side_street => "Kentucky Ave."
+			},
+			"Yellow" => {
+				:main_street => "Marvin Gardens"
+				:side_street => "Atlantic Ave."
+			},
+		},
+		:stream_line => {
+			"Blue" => {
+				:main_street => "Boardwalk"
+				:side_street => "Park Place"
+			},
+			"Green" => {
+				:main_street => "Pennsylvania Ave."
+				:side_street => "Pacific Ave."
+			},
+		},
+	}
 end
