9415acf9e1c8a669781dbcc491d799a838aeed50
Tanya Bihag <tbihag@gmail.com>
2019-08-09 03:56:39 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,30 +1,38 @@
-def base_hash
+def countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    }
   
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
-	return
+	
 end
 
-def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+def countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    }
 end
 
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+def countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    }
 end
 
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+def countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    }
 end
  
  
