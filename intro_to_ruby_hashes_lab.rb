4504e4deb25d34c9a3d5578fe21858d494faa5b1
Jeffrey Brown <brownjs03@gmail.com>
2019-08-09 13:23:59 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,20 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
-end
-
-def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-end
-
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-end
-
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-end
+ monopoly = {:colors => 
+ :purple => {
+ :first => "Mediterranean Ave",
+ :second => "Baltic Ave"},
+ :light_blue => {
+   :first => "Oriental Ave",
+ :second => "Vermont Ave",
+ :third => "Connecticut Ave"},
+ :pink => {
+   :first => "St. Charles Place",
+   :second => "States Ave",
+   :third => "Virginia Ave"},
+   :orange => {
+     :first => "St. James Place",
+     :second => "Tennessee Ave",
+     :third => "New York Ave"}
+   }
+   }
+ end 
---
+++ testingthis.rb
@@ -0,0 +1,25 @@
+def base_hash
+ monopoly = 
+ 
+end
+
+def monopoly_with_second_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+end
+
+def monopoly_with_third_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+end
+
+def monopoly_with_fourth_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+end
