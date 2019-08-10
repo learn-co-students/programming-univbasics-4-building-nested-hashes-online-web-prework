3aa099b176d55310471ac650e58e771cca0f8231
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-10 02:34:58 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,12 +21,11 @@ def monopoly_with_third_tier
        :two_pieces_owned => 50,
        :three_pieces_owned => 100,
        :four_pieces_owned => 200},
-	  },
 	    :names => {
        :reading_railroad => {},
        :pennsylvania_railroad => {},
        :b_and_o_railroad => {},
-       :shortline => {},
+       :shortline_railroad => {},
 	  }
    }
   }
@@ -41,12 +40,11 @@ def monopoly_with_fourth_tier
        :two_pieces_owned => 50,
        :three_pieces_owned => 100,
        :four_pieces_owned => 200},
-	  },
 	    :names => {
        :reading_railroad => {"mortgage_value" => "$100"},
        :pennsylvania_railroad => {"mortgage_value" => "$200"},
        :b_and_o_railroad => {"mortgage_value" => "$400"},
-       :shortline => {"mortgage_value" => "$800"},
+       :shortline_railroad => {"mortgage_value" => "$800"},
 	  }
    }
   }
