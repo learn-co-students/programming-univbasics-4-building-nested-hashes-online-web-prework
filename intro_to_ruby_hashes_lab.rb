ac59beb65352dc73082ac341d6c8f3677b59f68d
cedric e goddard <8ceg6@comcast.net>
2019-08-25 22:37:32 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
-def base_hash
+def base_hash {:railroads}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	
 end
 
 def monopoly_with_second_tier
