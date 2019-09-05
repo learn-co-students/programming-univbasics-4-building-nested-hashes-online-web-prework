aa02ebe1fb5b0f7c03a0fd31046e9ea2e12c2880
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-05 16:01:24 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require "pry"
 def base_hash
 	monopoly = {railroads:
 	{}
@@ -12,37 +13,53 @@ end
 
 def monopoly_with_third_tier
   
-  monopoly = {railroads: 
-    {
-    pieces: 4, 
-    rent_in_dollars: 
-      {
-      one_piece_owned: 25,
-      two_pieces_owned: 50, 
-      three_pieces_owned: 100, 
-      four_pieces_owned: 200}
-      }
+  monopoly = {
+    
+    railroads: {
+      pieces: 4, 
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50, 
+        three_pieces_owned: 100, 
+        four_pieces_owned: 200
+        },
+      names: {
+          reading_railroad: {},
+        pennsylvania_railroad: {},
+        b_and_o_railroad: {},
+        shortline_railroad: {}
+          
+        }
   }
+  }
+  #binding.pry 
 end
 
 def monopoly_with_fourth_tier
-    monopoly = {railroads: 
-    {
-    pieces: 4, 
-    rent_in_dollars: 
-      {
-      one_piece_owned: 25,
-      two_pieces_owned: 50, 
-      three_pieces_owned: 100, 
-      four_pieces_owned: 200},
-      {names: 
-        {
-        reading_railroad: {}, 
-        pennsylvania_railroad: {}, 
-        b_and_o_railroad: {}, 
-        shortline_railroad: {}
+    monopoly = {
+      railroads: {
+        pieces: 4, 
+        rent_in_dollars: {
+          one_piece_owned: 25,
+          two_pieces_owned: 50, 
+          three_pieces_owned: 100, 
+          four_pieces_owned: 200
+        },
+        names: {
+          reading_railroad: {
+            "mortgage_value" => "$100"
+          }, 
+          pennsylvania_railroad: {
+            "mortgage_value" => "$200"
+          }, 
+          b_and_o_railroad: {
+            "mortgage_value" => "$400"
+          }, 
+          shortline_railroad: {
+            "mortgage_value" => "$800"
+          }
         }
+      
       }
-      }
-  }
+    }
 end
