ac775402011e547f940325362f025c2f44eebe69
Jeffery Eckert <bromodo@gmail.com>
2019-09-16 14:10:01 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,10 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  base_hash = {
+  monopoly_with_second_tier = {
     :railroads => 
     {:pieces => 4}
-  
+  }
 end
 
 def monopoly_with_third_tier
@@ -21,19 +21,22 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly_with_third_tier = {
-    :railroads => {
+    :railroads => 
+    {
     :pieces => 4,
     :rent_in_dollars => {
       :one_piece_owned =>25, 
       :two_pieces_owned =>50, 
       :three_pieces_owned =>100, 
-      :four_pieces_owned =>200}
+      :four_pieces_owned =>200},
     :names => {
       :reading_railroad => {},
-      :pennsilvania_railroad => {}, 
+      :pennsylvania_railroad => {}, 
       :b_and_o_railroad => {}, 
-      :shortline_railroad => {}}
+      :shortline_railroad => {}
     }
+    }
+  }
 end
   
 
@@ -42,4 +45,29 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly_with_fourth_tier = {
+    :railroads => 
+    {
+    :pieces => 4,
+    :rent_in_dollars => {
+      :one_piece_owned =>25, 
+      :two_pieces_owned =>50, 
+      :three_pieces_owned =>100, 
+      :four_pieces_owned =>200},
+    :names => {
+      :reading_railroad => {
+        "mortgage_value" => "$100"
+      },
+      :pennsylvania_railroad => {
+        "mortgage_value" => "$200"
+      }, 
+      :b_and_o_railroad => {
+        "mortgage_value" => "$400"
+      }, 
+      :shortline_railroad => {
+        "mortgage_value" => "$800"
+      }
+    }
+    }
+  }
 end
