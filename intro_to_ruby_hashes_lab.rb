ba8e4027377e02dde73a50dba55bf25e8f86e30e
Jeffery Eckert <bromodo@gmail.com>
2019-09-16 13:36:22 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,12 +27,12 @@ def monopoly_with_third_tier
       :one_piece_owned =>25, 
       :two_pieces_owned =>50, 
       :three_pieces_owned =>100, 
-      :four_pieces_owned =>200},
+      :four_pieces_owned =>200}
     :names => {
       :reading_railroad => {},
       :pennsilvania_railroad => {}, 
       :b_and_o_railroad => {}, 
-      :shortline_railroad => {}
+      :shortline_railroad => {}}
     }
 end
   
