5895a282947c9f40be9df0e904ddd9e1c1797aac
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-25 23:13:02 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,10 +37,19 @@ def monopoly_with_fourth_tier
 	hash ={
 		:railroads => {
 			:names => {
-						:reading_railroad => {"mortage_value" => "$100"},
-						:pennsylvania_railroad => {},
-						:b_and_o_railroad => {},
-						:shortline_railroad => {}
+						:reading_railroad =>{
+							"mortgage_value" => "$100"
+						},
+
+						:pennsylvania_railroad => {
+							"mortgage_value" => "$200"
+						},
+						:b_and_o_railroad => {
+							"mortgage_value" => "$400"
+						},
+						:shortline_railroad => {
+							"mortgage_value" => "$800"
+						}
 				},
 			:rent_in_dollars => {
 						:one_piece_owned => 25.00,
