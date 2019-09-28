8368d2eedcfe9b8336680fda7a418077911d187d
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-09-28 17:34:06 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   hash = {}
-  hash[]
+  hash[key]
 end
 
 def monopoly_with_second_tier
