0f0e2a3a06d95a49ebb166e9f597dfe65bb632cd
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-28 17:55:11 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,15 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  person = {
+    name: "Bob"
+    age: "31"
+  }
 end
 
 def monopoly_with_second_tier
+  person =>
+    {name: "Bob", age: "31"}
+    {name: "Ken", age: "30"}
+  
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
