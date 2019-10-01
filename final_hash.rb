42cf467871ea919c991250cf2f212459092d74c4
Makeda P Mckune <makedamckune@gmail.com>
2019-10-01 03:35:28 +0000

    Done.

--- final_hash.rb
+++ final_hash.rb
@@ -1,19 +1,17 @@
-# The final hash will look like this. Earlier methods will expect less as
-# you're suppose to "build up" to this.
-
-monopoly =  {
-  :railroads=>
+monopoly_{: 
+ :railroads=>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces=> 20,
+    :rent_in_dollars=> 200.00
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
+      :four_pieces_owned=>200   },
     :names=>
     {
+      monopoly_{monopoly}
+
       :reading_railroad=>
       {
         "mortgage_value"=>"$100"
