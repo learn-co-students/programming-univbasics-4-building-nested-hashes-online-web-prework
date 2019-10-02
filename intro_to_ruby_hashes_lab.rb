cdcea1ccbdbc32b89c1e1d7c7c4a2d09b2abf64c
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-02 03:53:44 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	        :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
 	        :three_pieces_owned => 100,
-	        :four_pieces_owned => 100,
+	        :four_pieces_owned => 200,
 	       
 	      },
 	      :names => {
