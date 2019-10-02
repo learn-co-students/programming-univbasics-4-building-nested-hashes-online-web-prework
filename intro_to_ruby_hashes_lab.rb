cffb665195691069c758bce12b8d506705994dcb
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-02 03:53:03 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	      :rent_in_dollars => {
 	        :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
-	        :three_pieces_owned => 75,
+	        :three_pieces_owned => 100,
 	        :four_pieces_owned => 100,
 	       
 	      },
