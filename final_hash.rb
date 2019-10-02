242231ca1409bbb0bc5567d61e097b21498f822f
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-02 20:33:38 +0000

    Done.

--- final_hash.rb
+++ final_hash.rb
@@ -1,5 +1,5 @@
 # The final hash will look like this. Earlier methods will expect less as
-# you're suppose to "build up" to this.
+# you're suppose to "build up" to this..
 
 monopoly =  {
   :railroads=>
