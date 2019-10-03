712db153b93f8963870231b3c759c8b8671a6b4f
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-03 01:06:34 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
 	      :names => {
 	        :reading_railroad => {},
 	        :pennsylvania_railroad => {},
-	        :names3 => {},
+	        :b_and_o_railroad => {},
 	        :names4 => {}
 	        
 	      }
