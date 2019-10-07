e76cd579baad4b67f6f9a4f74db29a13245e6afa
William <teststuffreally@gmail.com>
2019-10-07 16:46:53 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,6 +3,9 @@ def base_hash
 end
 
 def monopoly_with_second_tier
+  hash= {:railroads => {
+    :pieces => 4
+  } } 
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -10,6 +13,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  hash= {:railroads => {
+    :pieces => 4
+  } } 
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
