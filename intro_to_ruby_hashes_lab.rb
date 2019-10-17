af16077c75cdb1d38373f6d9ee66ca3ef6dd6aeb
Kizito Njoku <nkizito57@gmail.com>
2019-10-17 17:42:08 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+monopoly = {
+	  :railroads => {}
+	 }
+	 monopoly
 end
 
 def monopoly_with_second_tier
@@ -9,6 +12,14 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = {
+	  :railroads => {
+	  :rent_in_dollars => {},
+	  :pieces => 4,
+	  :name => {}
+	 }
+  }
+	 
 end
 
 def monopoly_with_third_tier
@@ -16,6 +27,14 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = {
+	  :railroads => {
+	  :rent_in_dollars => 
+	  {:four_pieces_owned => 200, :three_pieces_owned => 100,:two_pieces_owned => 50, :one_piece_owned => 25},
+	  :pieces => 4,
+	  :names => {:reading_railroad =>{} ,:pennsylvania_railroad =>{},:b_and_o_railroad => {},:shortline_railroad => {}}
+	 }
+  }
 end
 
 def monopoly_with_fourth_tier
@@ -23,4 +42,22 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+ monopoly = {
+	  :railroads => {
+	  :rent_in_dollars => 
+	  {:four_pieces_owned => 200, :three_pieces_owned => 100,:two_pieces_owned => 50, :one_piece_owned => 25},
+	  :pieces => 4,
+	  :names => {:reading_railroad =>{
+	    "mortgage_value" => "$100"
+	  } ,:pennsylvania_railroad =>{
+	    "mortgage_value" => "$200"
+	  },:b_and_o_railroad => {
+	    "mortgage_value" => "$400"
+	  },:shortline_railroad => {
+	    "mortgage_value" => "$800"
+	  }, 
+	  
+	  }
+	 }
+  }
 end
