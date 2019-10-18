066841674a469916711da30401ea9280ee93b5d0
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-18 17:02:05 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,21 @@
 def base_hash
+monopoly = {
+	:railroads=>
+}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
 end
 
 def monopoly_with_second_tier
+	monopoly = {
+
+		 :railroads=>
+		 {
+			 :pieces=>4,
+			 :names=>{},
+			 :rent_in_dollars=>{}
+	}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -12,6 +23,25 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+	  monopoly = {
+			:railroads=>
+			{
+				:pieces=>4,
+				:names=>
+		{
+			:reading_railroad=>{},
+			:pennsylvania_railroad=>{},
+			:b_and_o_railroad=>{},
+			:shortline=>{}
+		},
+		:rent_in_dollars=>
+		{
+			:one_piece_owned=>25,
+			:two_pieces_owned=>50,
+			:three_pieces_owned=>100,
+			:four_pieces_owned=>200
+		}
+		}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -19,6 +49,38 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+	monopoly = {
+			:railroads=>
+			{
+			:pieces=>4,
+			:names=>
+			{
+				:reading_railroad=>
+				{
+					"mortgage_value"=>"$100"
+				},
+				:pennsylvania_railroad=>
+				{
+					"mortgage_value"=>"$200"
+				},
+				:b_and_o_railroad=>
+				{
+					"mortgage_value"=>"$400"
+				},
+				:shortline=>
+				{
+					"mortgage_value"=>"$800"
+				}
+			},
+			:rent_in_dollars=>
+			{
+				:one_piece_owned=>25,
+				:two_pieces_owned=>50,
+				:three_pieces_owned=>100,
+				:four_pieces_owned=>200
+			}
+			}
+		}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
