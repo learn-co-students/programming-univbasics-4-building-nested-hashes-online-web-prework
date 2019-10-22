101360f5c672f66bda787a5aa00869e38b1e5bd5
jerrard suell <seulljay@gmail.com>
2019-10-22 17:05:34 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  monopoly=	{
+    :railroads => {
+  
+    
+  }
 end
 
 def monopoly_with_second_tier
