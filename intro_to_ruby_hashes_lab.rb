7ddd254b4472ac950d4f537e2b5d382fccbb0028
connor worthen <connorw220@protonmail.com>
2019-10-28 03:23:22 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,42 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  capitals = {
+		:north_america => {
+			"california" => {
+				:capital => "Sacromento"
+				:capital_climate => "Hot"
+		}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	capitals = {
+		:california => {
+			"Cali" => {
+				:capital => "Sacromento"
+				:capital_climate => "Hot"
+		},
+		"oregon" => {
+			:capital => "Salem"
+			:capital_climate => "cold"
+		}
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	capitals = {
+		:california => {
+			"Cali" => {
+				:capital => "Sacromento"
+				:capital_climate => "Hot"
+		},
+		"oregon" => {
+			:capital => "Salem"
+			:capital_climate => "Cold"
+		}
+	},
+	:africa => {
+		"Ghana" => {
+			:capital => "Accra"
+			:capital_climate => "Hot"
+		}
 end
 
 def monopoly_with_fourth_tier
