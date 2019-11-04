b4d4e3b93fd1008964aa14f730c403b745a3fdba
outstanding-reference-3248 <omar.benjamin377@gmail.com>
2019-11-04 01:36:59 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+	{:north_america => {
+	 "Canada" => {
+		 :capital => "Ottawa",
+		 :capital_climate => "Kőppen Dfb"}
+
 end
 
 def monopoly_with_second_tier
