6bf708ac3ac5c16f75e495e2062382b90dd81ded
dwane richards <dwanerich@gmail.com>
2019-11-11 17:52:42 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,16 +22,15 @@ def monopoly_with_third_tier
   
   monopoly = {:railroads =>
   {:rent_in_dollars => 
-  {:four_pieces_owned => 200,
-  :three_pieces_owned => 100,
+  {:one_piece_owned => 25,
   :two_pieces_owned => 50,
-  :one_piece_owned => 25
-  },
+  :three_pieces_owned => 100,
+  :four_pieces_owned => 200},
   :names => {
   :reading_railroad => {},
-  :robbie => "Benedict Man",
-  :max => "Well Baby",
-  :dylan => "willy b"}
+  :pennsylvania_railroad => {},
+  :b_and_o_railroad => {},
+  :shortline_railroad => {}}
   }}
   
   
@@ -45,4 +44,19 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  monopoly = {:railroads =>
+  {:rent_in_dollars => 
+  {:one_piece_owned => 25,
+  :two_pieces_owned => 50,
+  :three_pieces_owned => 100,
+  :four_pieces_owned => 200},
+  :names => {
+  :reading_railroad => {"mortgage_value"=>"$100"},
+  :pennsylvania_railroad => {"mortgage_value" =>"$200"},
+  :b_and_o_railroad =>{"mortgage_value"=>"$400"},
+  :shortline_railroad=>{"mortgage_value" => "$800"}}}}
+  
+  
+  
 end
