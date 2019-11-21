c9cfcdcaf71d70c72d62e7bace79c6f898c324d2
Austin Johnson <technicallyaustin@gmail.com>
2019-11-21 01:31:06 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,24 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  # keyword "return." Nice and neat
+  
+	  my_first_hash = {:railroads => {}}
+	  my_first_hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
+  
+   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+   base_hash
+  my_second_hash = {
+    :railroads => {pieces: 4}
+    }
+    
+  
 end
 
 def monopoly_with_third_tier
@@ -16,6 +26,19 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  monopoly_with_second_tier
+  my_third_hash = {
+    :railroads => {
+      rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
+      names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
+      
+    
+  
+      
+      
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
@@ -23,4 +46,20 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  monopoly_with_third_tier
+  my_fourth_hash = {
+    :railroads => {
+      rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
+      names: {
+        :reading_railroad => {"mortgage_value" => "$100"},
+        :pennsylvania_railroad => {"mortgage_value"=> "$200"},
+        :b_and_o_railroad => {"mortgage_value" => "$400"},
+        :shortline_railroad => {"mortgage_value" =>"$800"}}
+    
+    
+      
+    }
+  }
+  
 end
