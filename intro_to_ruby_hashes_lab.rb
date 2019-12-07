b8ad7ffdf7b2bfd84a122b32c2c24cc580adea54
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-07 00:14:28 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,9 +53,9 @@ def monopoly_with_fourth_tier
         }, 
         :names => {
           :reading_railroad => {"mortgage_value" => "$100"},
-          :pennsylvania_railroad => {},
-          :b_and_o_railroad => {},
-          :shortline_railroad => {}
+          :pennsylvania_railroad => {"mortgage_value" => "$200"},
+          :b_and_o_railroad => {"mortgage_value" => "$400"},
+          :shortline_railroad => {"mortgage_value" => "$800"}
             
           
         }
