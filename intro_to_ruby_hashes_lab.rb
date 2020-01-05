a9fdbf0c30c64347ef62b72cfe94e7c739029211
fadimata tillman <fadi.tillman@gmail.com>
2020-01-05 20:03:36 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,82 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  base_hash = {                            
+    :base_first_categorie => {
+      "undercategorieone" => {
+        :categoriea => "undercategoriea"
+        :categorieb => "undecategorieb"
+      },
+      "undercategorietwo" => { 
+        :categoriea => "undercategorieatwo"
+        :categorieb => "undercategoriebtwo"
+    }
+    },
+    :base_second_categorie => {
+      "undercategoriethree" => {
+        :categoriea => "undercategorieathree"
+        :categorieb => "undercategoriebthree"
+      },
+      "undercategoriefour" => {
+        :categoriea => "undercategorieafour"
+        :categorieb => "undercategoriebfour"
+      }
+    }
+  }
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly_with_second_tier ={
+    :base_first_categorie => {
+      "undercategorieone" => {
+        :categoriea => "undercategoriea"
+        :categorieb => "undecategorieb"
+      },
+      "undercategorietwo" => { 
+        :categoriea => "undercategorieatwo"
+        :categorieb => "undercategoriebtwo"
+  }
+
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly_with_third_tier = {
+    :base_first_categorie => {
+      "undercategorieone" => {
+        :categoriea => "undercategoriea"
+        :categorieb => "undecategorieb"
+      },
+      "undercategorietwo" => { 
+        :categoriea => "undercategorieatwo"
+        :categorieb => "undercategoriebtwo"
+    }
+    },
+    :base_second_categorie => {
+      "undercategoriethree" => {
+        :categoriea => "undercategorieathree"
+        :categorieb => "undercategoriebthree"
+      },
+  }
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly_with_fourth_tier = {
+   :base_first_categorie => {
+      "undercategorieone" => {
+        :categoriea => "undercategoriea"
+        :categorieb => "undecategorieb"
+      },
+      "undercategorietwo" => { 
+        :categoriea => "undercategorieatwo"
+        :categorieb => "undercategoriebtwo"
+    }
+    },
+    :base_second_categorie => {
+      "undercategoriethree" => {
+        :categoriea => "undercategorieathree"
+        :categorieb => "undercategoriebthree"
+      },
+      "undercategoriefour" => {
+        :categoriea => "undercategorieafour"
+        :categorieb => "undercategoriebfour"
+      } 
+  }
 end
