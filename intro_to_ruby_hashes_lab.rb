f162a1a4dead57463b5cbab96c0ea2953a118ad8
Craig Storer <craigstorer@gmail.com>
2020-02-23 08:43:22 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,8 @@ def monopoly_with_third_tier
   monopoly = {:railroads => {:pieces => 4, :rent_in_dollars=> {:one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200},:names => {
+      :four_pieces_owned=>200},
+      :names => {
       :reading_railroad=>
       { 
         
@@ -45,18 +46,25 @@ def monopoly_with_fourth_tier
   monopoly = {:railroads => {:pieces => 4, :rent_in_dollars=> {:one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200},:names => {
+      :four_pieces_owned=>200},:names=>
+    {
       :reading_railroad=>
-      { 
-        
+      {
+        "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
-        
+        "mortgage_value"=>"$200"
       },
       :b_and_o_railroad=>
       {
-        
+        "mortgage_value"=>"$400"
       },
-      :shortline_railroad=>{}}}}
+      :shortline_railroad=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 end
